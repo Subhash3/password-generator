bd3b5a54fc72082b9fa9ccc0a756ab2bb157af74
Subhash Sarangi <subhashsarangi123@gmail.com>
2020-07-18 13:13:15 +0530

    Complete Working Version

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
+import GenerateButton from './components/GenerateButton'
+import PasswordBox from './components/PasswordBox'
+import copy from 'copy-to-clipboard';
+import './styles/App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Password Generator</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      password: "Password"
+    }
+  }
+
+  componentDidMount = () => {
+    this.generatePassword()
+  }
+
+  generatePassword = () => {
+    let passwordLength = getRandomInteger(10, 16)
+    let password = ""
+    for (let i = 0; i < passwordLength; i++) {
+      let randNum = getRandomInteger(33, 126)
+      let asciiChar = String.fromCharCode(randNum)
+      password += asciiChar
+    }
+    this.setState({ password })
+  }
+
+  copyHandler = () => {
+    copy(this.state.password)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <div className="title">Password Generator</div>
+          <PasswordBox value={this.state.password} copyHandler={this.copyHandler} />
+          <GenerateButton generatePassword={this.generatePassword} />
+        </div>
+      </div>
+    );
+  }
+}
+
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 export default App;
